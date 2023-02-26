4315017248f607de33acef2e07883be7f2edc88f
Drouz <orieldelgado01@gmail.com>
2023-02-26 14:45:39 -0500

    Palindrome.java has been modified with regular expressions

--- DATA STRUCTURES/LINEAR DATA STRUCTURES/Arrays/src/SingleDimensionalArray/Palindrome.java
+++ DATA STRUCTURES/LINEAR DATA STRUCTURES/Arrays/src/SingleDimensionalArray/Palindrome.java
@@ -1,31 +1,32 @@
 package SingleDimensionalArray;
 import java.util.Scanner;
+import java.util.regex.*;
 public class Palindrome {
+    static Pattern pattern = Pattern.compile("[a-z]", Pattern.CASE_INSENSITIVE);
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String word, message;
         char [] letters;
         int countLetters;
 
-        System.out.println("Enter your word > " );
+        System.out.println("Enter your word:" );
         word = sc.nextLine().toLowerCase();
 
         countLetters = countLetters(word.trim());
         letters = decompose(word, countLetters);
         message = checkWord(letters, countLetters) ? "The word is palindrome" : "The word is not palindrome";
 
-        System.out.println("Word ---> " + word + "\n " + message);
+        System.out.println("Word ---> " + String.valueOf(letters) + "\n " + message);
     }
 
     public static int countLetters(String word){
         int count = 0;
 
-        for(int i = 0; i < word.length(); i++)
-            if(word.charAt(i) > 96  && word.charAt(i) < 123)
-                count ++;
-
+        for(int i = 0; i < word.length(); i++){
+            if(pattern.matcher(String.valueOf(word.charAt(i))).matches()) count++;
+        }
         return count;
-    }//add for Uppercase condition
+    }
 
     public static char[] decompose(String word, int nChar){
         //String decomposeWord = "";
@@ -35,7 +36,7 @@ public class Palindrome {
 
 
         while(add != nChar){
-            if(word.charAt(i) > 96  && word.charAt(i) < 123){
+            if(pattern.matcher(String.valueOf(word.charAt(i))).matches()){
                 letters[add] = word.charAt(i);
                 add++;
             }
@@ -51,7 +52,6 @@ public class Palindrome {
         char [] reverseWordLetter = new char[nChar];
         int count = 0;
 
-
         for (int i = (letters.length - 1); i >= 0; i--) {
             reverseWordLetter[count] = letters[i];
             count++;
@@ -61,7 +61,6 @@ public class Palindrome {
 
     public static boolean reviewEquality(char[] word, char [] wordReverse){
         int i = 0;
-
         while (i < word.length){
             if (word[i] == wordReverse[i]) {
                 i++;
